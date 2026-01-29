1eeff23c5ce1f462ae96d29da5f211126e045575
Salman <eng.salman01@outlook.com>
2026-01-29 22:19:21 +0300

    chore: add Toaster to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
         {/* Vercel Analytics */}
         <Analytics />
